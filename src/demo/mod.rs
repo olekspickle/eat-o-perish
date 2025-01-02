0528f05808246be3812d46480f3c1a1a57ac875b
Alec Deason <alec@tinycountry.com>
2025-01-01 18:10:20 -0800

    Initial blenvy/avian integration

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -5,16 +5,14 @@
 
 use bevy::prelude::*;
 
-mod animation;
 pub mod level;
-mod movement;
-pub mod player;
+
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+struct Player;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((
-        animation::plugin,
-        movement::plugin,
-        player::plugin,
         level::plugin,
     ));
 }
