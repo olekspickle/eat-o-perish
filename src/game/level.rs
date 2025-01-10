96503a91ed6489cbacc93b56f5853758f1d0e1d6
Alec Deason <alec@tinycountry.com>
2025-01-10 10:45:43 -0800

    Nutrient rain

--- src/game/level.rs
+++ src/game/level.rs
@@ -1,15 +1,24 @@
-//! Spawn the main level.
+use std::time::Duration;
 
-use rand::Rng;
+use rand::prelude::*;
 
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    time::common_conditions::on_timer,
+};
 use blenvy::*;
 
 use crate::game::critters::{FoodPellet, Herbivore, Preditor};
 
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+pub struct FloorPlate;
+
 pub(super) fn plugin(app: &mut App) {
+    app.register_type::<FloorPlate>();
     app.add_systems(Update, (
         spawn_food_pellet,
+        food_pellet_rain.run_if(on_timer(Duration::from_millis(10))),
     ));
 }
 
@@ -59,5 +68,30 @@ fn spawn_food_pellet(
             HideUntilReady,
             AddToGameWorld,
         ));
+    }
+}
+
+fn food_pellet_rain(
+    mut commands: Commands,
+    existing_pellets: Query<Entity, With<FoodPellet>>,
+    floor_plates: Query<(Entity, &GlobalTransform), With<FloorPlate>>,
+    children: Query<&Children>,
+    aabs: Query<&bevy::render::primitives::Aabb>,
+) {
+    if existing_pellets.iter().count() > 2000 {
+        return
+    }
+    let mut rng = rand::thread_rng();
+    if let Some((entity, transform)) = floor_plates.iter().choose(&mut rng) {
+        for child in children.iter_descendants(entity) {
+            if let Ok(aab) = aabs.get(child) {
+                let location = transform.transform_point(Vec3::new(rng.gen_range(-aab.half_extents.x..aab.half_extents.x), 20.0, rng.gen_range(-aab.half_extents.z..aab.half_extents.z)) + Vec3::from(aab.center));
+                commands.spawn((
+                    FoodPellet,
+                    Transform::from_translation(location),
+                ));
+                return
+            }
+        }
     }
 }
